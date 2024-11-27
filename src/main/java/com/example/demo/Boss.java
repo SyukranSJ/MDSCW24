14a1de92c77f94c7e736c784ab2079258b8ee72b
SyukranSJ <syukransj@gmail.com>
2024-11-27 13:31:26 +0800

    Added Health Bar to Boss.java Added Shield Movement to Boss.java Added health bar color effect that react based on the health of the boss move shield declaration from level view two to the top of the class

--- src/main/java/com/example/demo/Boss.java
+++ src/main/java/com/example/demo/Boss.java
@@ -2,38 +2,49 @@ package com.example.demo;
 
 import java.util.*;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 public class Boss extends FighterPlane {
 
 	private static final String IMAGE_NAME = "bossplane.png";
 	private static final double INITIAL_X_POSITION = 1000.0;
 	private static final double INITIAL_Y_POSITION = 400;
-	private static final double PROJECTILE_Y_POSITION_OFFSET = 75.0;
-	private static final double BOSS_FIRE_RATE = .04;
-	private static final double BOSS_SHIELD_PROBABILITY = .002;
+	private static final double PROJECTILE_Y_POSITION_OFFSET = 75;
+	private static final double BOSS_FIRE_RATE = 0.001;
+	private static final double BOSS_SHIELD_PROBABILITY = .02;
 	private static final int IMAGE_HEIGHT = 90;
 	private static final int VERTICAL_VELOCITY = 8;
-	private static final int HEALTH = 100;
+	private static final int HEALTH = 10;
 	private static final int MOVE_FREQUENCY_PER_CYCLE = 5;
 	private static final int ZERO = 0;
 	private static final int MAX_FRAMES_WITH_SAME_MOVE = 10;
-	private static final int Y_POSITION_UPPER_BOUND = -100;
-	private static final int Y_POSITION_LOWER_BOUND = 475;
-	private static final int MAX_FRAMES_WITH_SHIELD = 500;
+	private static final int Y_POSITION_UPPER_BOUND = 0;
+	private static final int Y_POSITION_LOWER_BOUND = 525;
+	private static final int MAX_FRAMES_WITH_SHIELD = 100;
 	private final List<Integer> movePattern;
 	private boolean isShielded;
 	private int consecutiveMovesInSameDirection;
 	private int indexOfCurrentMove;
 	private int framesWithShieldActivated;
+	private final ShieldImage shieldImage;
+    private Rectangle healthBar;
+    private int currentHealth;
 
 	public Boss() {
 		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, HEALTH);
 		movePattern = new ArrayList<>();
+		shieldImage = new ShieldImage(INITIAL_X_POSITION, INITIAL_Y_POSITION);
 		consecutiveMovesInSameDirection = 0;
 		indexOfCurrentMove = 0;
 		framesWithShieldActivated = 0;
 		isShielded = false;
+		currentHealth = HEALTH;
+        createHealthBar();
 		initializeMovePattern();
 	}
+
+	
 
 	@Override
 	public void updatePosition() {
@@ -42,13 +53,21 @@ public class Boss extends FighterPlane {
 		double currentPosition = getLayoutY() + getTranslateY();
 		if (currentPosition < Y_POSITION_UPPER_BOUND || currentPosition > Y_POSITION_LOWER_BOUND) {
 			setTranslateY(initialTranslateY);
+
 		}
+		updateShieldPosition();  // Update the shield position after the boss moves
 	}
 	
+	private void updateShieldPosition() {
+		shieldImage.setLayoutX(getLayoutX() + getTranslateX());
+		shieldImage.setLayoutY(getLayoutY() + getTranslateY() - 40);
+	}
+
+
 	@Override
 	public void updateActor() {
 		updatePosition();
-		//updateShield(); // This will toggle the shield visibility
+		updateShield(); // This will toggle the shield visibility
 		//updateHealthBar(); // This will update the health bar width based on health
 	}
 
@@ -60,10 +79,38 @@ public class Boss extends FighterPlane {
 	
 	@Override
 	public void takeDamage() {
-		if (!isShielded) {
-			super.takeDamage();
-		}
-	}
+        if (!isShielded) {
+            super.takeDamage();
+            currentHealth -= 1;  // damage reduce health by 1
+            if (currentHealth < 0) {
+                currentHealth = 0;
+            }
+            updateHealthBar();  // Update the health bar when damage is taken
+        }
+    }
+
+	private void createHealthBar() {
+        // Create the health bar with initial full width
+        healthBar = new Rectangle(1000, 10, currentHealth, 20);  // X, Y, width, height
+        healthBar.setFill(Color.GREEN);  // Set the initial color to green
+    }
+
+    public Rectangle getHealthBar() {
+        return healthBar;
+    }
+
+	public void updateHealthBar() {
+        // Update the health bar width based on current health
+        healthBar.setWidth(currentHealth);
+        if (currentHealth > 10) {
+            healthBar.setFill(Color.GREEN);
+        } else if (currentHealth > 5) {
+            healthBar.setFill(Color.YELLOW);
+        } else {
+            healthBar.setFill(Color.RED);
+
+        }
+    }
 
 	private void initializeMovePattern() {
 		for (int i = 0; i < MOVE_FREQUENCY_PER_CYCLE; i++) {
@@ -75,12 +122,22 @@ public class Boss extends FighterPlane {
 	}
 
 	private void updateShield() {
-		if (isShielded) framesWithShieldActivated++;
-		else if (shieldShouldBeActivated()) activateShield();	
-		if (shieldExhausted()) deactivateShield();
+		if (isShielded) {
+			framesWithShieldActivated++;
+			shieldImage.showShield();
+			updateShieldPosition();
+		}
+		else if (shieldShouldBeActivated()) {
+			activateShield();	
+			updateShieldPosition();
+			
+		}
+		if (shieldExhausted()) {
+			
+			deactivateShield();
+			
+		}
 	}
-
-	
 
 	private int getNextMove() {
 		int currentMove = movePattern.get(indexOfCurrentMove);
@@ -114,11 +171,18 @@ public class Boss extends FighterPlane {
 
 	private void activateShield() {
 		isShielded = true;
+		//shieldImage.showShield();
 	}
 
 	private void deactivateShield() {
 		isShielded = false;
+		shieldImage.hideShield();
 		framesWithShieldActivated = 0;
 	}
+
+	public ShieldImage getShieldImage() {
+		return shieldImage;
+	}
+
 
 }
